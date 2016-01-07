ad341ea2cc2671862150e0c688a28589f033a69b
Luis Covilla <luis.covilla@gmail.com>
2016-01-07 15:49:04 -0500

    updated with pico san
    pico made the connection with gitHub so the projects can be shown.

--- js/init.js
+++ js/init.js
@@ -3,6 +3,21 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+    $.getJSON( "https://api.github.com/users/exilesas/repos", function(data){
+    	var model = $("#product-model");
+    	$.each(data, function(index, value){
+    		console.log(index, value);
+    		var clone = model.clone();
+    		clone.appendTo("#products-container");
+    		clone.removeAttr("id");
+    		clone.find("h4").html(value.name);
+    		if(value.language==null){
+    			value.language = "Software"
+    		}
+    		clone.find("h5").html(value.language);
+    		clone.find("p").html(value.description);
+    	});
+    	model.remove();
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
